9888b1a02c709e737fd6a991627bfeb442b91d7f
Younez <184885@stud.hvl.no>
2026-02-03 11:55:18 +0100

    oppsatt basisprosjekt

--- oblig1.v3.sql
+++ oblig1.v3.sql
@@ -3,24 +3,33 @@ DROP SCHEMA IF EXISTS dat109_o1 CASCADE;
 CREATE SCHEMA dat109_o1;
 SET search_path TO dat109_o1;
 
+CREATE TABLE brett(
+  id SERIAL PRIMARY KEY,
+  navn  VARCHAR(15) UNIQUE
+);
+
 CREATE TABLE dtype(
     id INT PRIMARY KEY,
     navn VARCHAR(15) UNIQUE);
 
 CREATE TABLE rute(
     ruteID INT PRIMARY KEY,
+    brettID INT NOT NULL,
     rute_type INT NOT NULL DEFAULT 1,
+    FOREIGN KEY (brettID) REFERENCESbrett(id) DEFAULT 1,
     FOREIGN KEY (rute_type) REFERENCES dtype(id));
 
 CREATE TABLE flytt(
-    start INT,
-    slutt INT,
+    start INT PRIMARY KEY,
+    slutt INT NOT NULL,
     flytt_type INT NOT NULL DEFAULT 8,
-    PRIMARY KEY (start, slutt),
     FOREIGN KEY (start) REFERENCES rute(ruteID),
     FOREIGN KEY (slutt) REFERENCES rute(ruteID),
     FOREIGN KEY (flytt_type) REFERENCES dtype(id)
     );
+
+INSERT INTO brettID
+VALUES(1,'hovudbrett');
 
 INSERT INTO dtype
 VALUES(1,'vanlig'),
@@ -32,7 +41,7 @@ VALUES(1,'vanlig'),
 (7,'slange'),
 (8,'stige');
 
-INSERT INTO rute
+INSERT INTO rute(ruteID, rute_type)
 VALUES(0, 3),
 (1,2),
 (100,4);
